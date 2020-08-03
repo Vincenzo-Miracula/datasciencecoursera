252f598a5b7620116799f9d6a30f67823392884b
Vincenzo-Miracula <65777210+Vincenzo-Miracula@users.noreply.github.com>
2020-08-03 12:05:06 +0200

    Update plot1.R

--- Exploratory Data Analysis/project2/plot1.R
+++ Exploratory Data Analysis/project2/plot1.R
@@ -4,7 +4,9 @@ path <- getwd()
 download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
               , destfile = paste(path, "dataFiles.zip", sep = "/"))
 unzip(zipfile = "dataFiles.zip")
-
+########################################
+########################################
+#plo1
 SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
 NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
 
