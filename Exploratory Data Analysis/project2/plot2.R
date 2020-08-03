598e605272a1727dd26644b3ff62b91061a55007
Vincenzo-Miracula <65777210+Vincenzo-Miracula@users.noreply.github.com>
2020-08-03 12:06:01 +0200

    Create plot2.R

---
+++ Exploratory Data Analysis/project2/plot2.R
@@ -0,0 +1,22 @@
+library("data.table")
+library(ggplot2)
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+########################################
+########################################
+#plot2
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
+
+png(filename='plot2.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
